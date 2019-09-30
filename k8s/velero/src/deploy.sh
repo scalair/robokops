d975dfcc791a5e530a47b055eb5c49e4faaeae05
Maxime Renou <maxime.renou.50@gmail.com>
2019-09-30 15:53:47 +0200

    [Velero] Remove commented line in deploy.sh

--- k8s/velero/src/deploy.sh
+++ k8s/velero/src/deploy.sh
@@ -2,11 +2,3 @@
 kubectl apply -f manifests/ns.yaml
 
 helm upgrade --install --force --namespace velero -f velero/values.yaml velero stable/velero --version 2.1.6 --wait
-
-# velero install \
-#     --provider aws \
-#     --bucket eks.stage.scalair.eu-west-1 \
-#     --prefix velero \
-#     --secret-file ./credentials-velero \
-#     --backup-location-config region=eu-west-1 \
-#     --snapshot-location-config region=eu-west-1
