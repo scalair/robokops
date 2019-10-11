ae04ead916c35f9625c2e21b7068433f983d4f62
blakelead <adel.abdelhak.lille@gmail.com>
2019-10-11 14:37:11 +0200

    Allow for cluster-init feature to apply all given manifests

--- k8s/cluster-init/CHANGELOG.md
+++ k8s/cluster-init/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+
+## 0.1.1 - 2019-10-11
+### Changed
+- deploy and delete scripts now apply all manifests contained in manifests directory
+
 ## 0.1.0 - 2019-09-09
 ### Added
 - Initial commit
--- k8s/cluster-init/src/delete.sh
+++ k8s/cluster-init/src/delete.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-kubectl delete -f manifests/rbac-admin.yaml
+kubectl delete -f manifests
 kubectl delete deployment tiller-deploy --namespace kube-system
 kubectl delete service tiller-deploy --namespace kube-system
-
-if [ -f /conf/cluster-init/manifests/kube-system-limit-range.yaml ]; then
-	kubectl delete -f /conf/cluster-init/manifests/kube-system-limit-range.yaml
-fi
-
-if [ -f /conf/cluster-init/manifests/storage-class.yaml ]; then
-	kubectl delete -f /conf/cluster-init/manifests/storage-class.yaml
-fi
