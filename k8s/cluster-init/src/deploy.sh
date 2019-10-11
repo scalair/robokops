daeb41ef86cca8e06dc248008e7813b003f3e4e8
Adel Abdelhak <aabdelhak@scalair.fr>
2019-10-11 14:37:11 +0200

    allow for cluster-init feature to apply all given manifests

--- k8s/cluster-init/src/deploy.sh
+++ k8s/cluster-init/src/deploy.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-kubectl apply -f manifests/rbac-admin.yaml
+kubectl apply -f manifests
 helm init --service-account admin --upgrade --wait
-
-if [ -f /conf/cluster-init/manifests/kube-system-limit-range.yaml ]; then
-	kubectl apply -f /conf/cluster-init/manifests/kube-system-limit-range.yaml
-fi
-
-if [ -f /conf/cluster-init/manifests/storage-class.yaml ]; then
-	kubectl apply -f /conf/cluster-init/manifests/storage-class.yaml
-fi
