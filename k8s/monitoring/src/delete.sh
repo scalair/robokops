35c1fee3f86c2069818e84701949747151f918b9
blakelead <adel.abdelhak.lille@gmail.com>
2020-04-27 11:02:40 +0200

    Delete pvc when deleting feature

--- k8s/monitoring/src/delete.sh
+++ k8s/monitoring/src/delete.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 ./build.sh
 
-# Extract the namespace manifest first, so we don't delete the manifests before everything else
+# Extract the namespace manifest first, so we don't delete the namespace before everything else
 mv manifests/00namespace-namespace.yaml .
 
 kubectl delete -f manifests/
+
+kubectl -n monitoring delete --all pvc
 
 # Finally, the namespace can be deleted
 kubectl delete -f 00namespace-namespace.yaml
